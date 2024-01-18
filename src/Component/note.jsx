1b18652b3cf7a7678a9e0344857fbaf261f027d0
abhijanbarik <cdhyper22@gmail.com>
2024-01-18 17:19:26 +0530

    Keeper-App-Testing

---
+++ src/Component/note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return(<div className="note">
+        <h1>Note title</h1>
+        <p>Type your content here.</p>
+    </div>)
+}
+
+export default Note;
